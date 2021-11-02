7985b6b3fa6783500dbe8adbad17c24f1fb02e9b
olivias1082 <olivia.singletary@selu.edu>
2021-11-02 16:57:56 -0500

    form example ish

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use vial::prelude::*;
 
 routes! {
-    GET "/info" => |req| format!(
-        "<p>Name: {}</p>", req.query("name").unwrap_or("None")
-    );
-    GET "/" => index;
+ 
+    GET "/" => |_| r#"
+<h3>vial form</h3>
+<form method="POST" action="/">
+    <p><label>Name: <input type="text" name="name"/></label></p>
+    <p><label>Post:
+    <input type="textarea" name="post"/>
+    </label></p>
+    <p><input type="submit"/></p>
+</form>
+    "#;
+    POST "/" => |req| format!(r#"
+    <h3>results</h3>
+    <p><b>Name:</b> {}</p>
+    <p><b>Post:</b> {}</p>
+    "#,
+            req.form("name").unwrap_or(""),
+            req.form("post").unwrap_or(""),
+        );
+    
 }
 
-fn index(req: Request) -> impl Responder {
-    "<form method='GET'>
-        <p>Enter your name: <input type='text' name='name'/></p>
-        <input type='submit'/>
-    </form>"
-}
+
 
 fn main() {
     run!("localhost:2000");
