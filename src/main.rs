dc8ec01b0c45d9ae3acfd5fa9a0b01d4541f5aa0
olivias1082 <olivia.singletary@selu.edu>
2021-11-09 09:03:39 -0600

    workinprogress vial thread set up without users so far

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,107 @@
 use vial::prelude::*;
-
-
+use mysql::*;
+use mysql::prelude::*;
 routes! {
+    GET "/echo" => echo;
+    POST "/echo" => post;
+}
+
+fn echo(_: Request) -> impl Responder {
+    "<form method='POST' >
+    <div class='top-bar'>
+        <h1>
+            Rust Forum
+        </h1>
+    </div>
+    <li class='row'>
+        <a href='/'></a>
+        <div class='header'>
+            <h4 class='title'>
+                Thread 1
+            </h4>
+        </div>
+        <div class='bottom'>
+            <p class='timestamp'>11/08/2021</p>
+            <p class='comment-count'>7 comments</p>
+        </div>
+    </li>
+    <li class='row'>
+        <a href='/item'></a>
+        <div class='header'>
+            <h4 class='title'>
+                Thread 2
+            </h4>
+        </div>
+        <div class='bottom'>
+            <p class='timestamp'>11/08/2021</p>
+            <p class='comment-count'>7 comments</p>
+        </div>
+    </li>
+    <input type=textarea name='echo'></input>
+    <button input='submit' name='thread'>Post Comment</button>
+    <div class='top-comment'>
+        <p class='user'>Corey T.</p>
+        <p class='timestamp'>10/10/2021</p>
+    </div>
+    <div class='comment-content'>Comment text here</div>
+</form>
+
+<style>
+body {
+    margin: 10px 60px;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+h4 {
+    margin: 0;
+}
+
+p {
+    margin: 5px 0;
+}
+
+.top-bar {
+    background-color: pink;
+    padding: 0 40px;
+}
+
+.main {
+    background-color: #F6F6EF;
+}
+
+.row {
+    padding: 5px 0;
+}
+
+.bottom {
+    display: flex;
+    color: grey;
+    font-size: 12px;
+}
+
+.timestamp {
+    padding-right: 10px;
+}
+</style>
+</body>
+
+"
+}
+
+fn post(req: Request) -> impl Responder {
+    format!(
+        "<h1>{}</h1>",
+        req.form("echo").unwrap_or("You didn't say anything!")
+    );
+    "INSERT INTO thread (echo)
+    VALUES (:echo)"
   
-    GET "/" => new;
-
 }
-
-fn new(_req: Request) -> impl Responder {
-    "<head>
-    <meta charset='UTF-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Forum Homepage</title>
-</head>
-
-<body>
-    <ol>
-    <form method='POST' >
-                <div class='top-bar'>
-                    <h1>
-                        Rust Forum
-                    </h1>
-                </div>
-                <li class='row'>
-                    <a href='/'></a>
-                    <div class='header'>
-                        <h4 class='title'>
-                            Thread 1
-                        </h4>
-                    </div>
-                    <div class='bottom'>
-                        <p class='timestamp'>11/08/2021</p>
-                        <p class='comment-count'>7 comments</p>
-                    </div>
-                </li>
-                <li class='row'>
-                    <a href='/item'></a>
-                    <div class='header'>
-                        <h4 class='title'>
-                            Thread 2
-                        </h4>
-                    </div>
-                    <div class='bottom'>
-                        <p class='timestamp'>11/08/2021</p>
-                        <p class='comment-count'>7 comments</p>
-                    </div>
-                </li>
-                <textarea></textarea>
-                <button input='submit'>Post Comment</button>
-                <div class='top-comment'>
-                    <p class='user'>Corey T.</p>
-                    <p class='timestamp'>10/10/2021</p>
-                </div>
-                <div class='comment-content'>Comment text here</div>
-            </form>
-        
-        <style>
-            body {
-                margin: 10px 60px;
-            }
-    
-            a {
-                text-decoration: none;
-                color: black;
-            }
-    
-            h4 {
-                margin: 0;
-            }
-    
-            p {
-                margin: 5px 0;
-            }
-    
-            .top-bar {
-                background-color: pink;
-                padding: 0 40px;
-            }
-    
-            .main {
-                background-color: #F6F6EF;
-            }
-    
-            .row {
-                padding: 5px 0;
-            }
-    
-            .bottom {
-                display: flex;
-                color: grey;
-                font-size: 12px;
-            }
-    
-            .timestamp {
-                padding-right: 10px;
-            }
-        </style>
-        </body>
-
-        "
-}
-
-
-
 
 fn main() {
-    run!("localhost:2000" );
+    run!("localhost:2000");
 }
--- tables.sql
+++ tables.sql
@@ -1,6 +1,6 @@
 SET GLOBAL SQL_MODE = "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION";  
-DROP TABLE IF EXISTS message;
 DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS thread;
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
  id int(16) PRIMARY KEY AUTO_INCREMENT NOT NULL, 
@@ -11,16 +11,16 @@ CREATE TABLE user (
      UNIQUE KEY username (username),
     UNIQUE KEY email (email)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-CREATE TABLE message (
+CREATE TABLE comment (
  id varchar(32) PRIMARY KEY NOT NULL,
-     comment_id varchar(32) NOT NULL,
+     thread_id varchar(32) NOT NULL,
     from_user_id int(16) NOT NULL,
     to_user_id int(16) NOT NULL,
     status tinyint(2) unsigned DEFAULT 1,
     create_time datetime NOT NULL,
 
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
-CREATE TABLE comment (
+CREATE TABLE thread (
   id varchar(32) PRIMARY KEY NOT NULL,
     user_id int(16) NOT NULL,
         status tinyint(2) unsigned DEFAULT 1,
